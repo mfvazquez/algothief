e11b67464726579aa998a1765627f56d3c091d62
fedetorlaschi@gmail.com <fedetorlaschi@gmail.com@c9f71e97-3780-c71b-477c-e2c6ad914a31>
2014-06-12 20:18:40 +0000

    complete bastante la clase tiempo

--- src/control/Tiempo.java
+++ src/control/Tiempo.java
@@ -1,9 +1,54 @@
 package control;
 
+import java.util.ArrayList;
+
 public class Tiempo {
-	public int hora;
-	public String dia;
+	private int hora;
+	private String[] dias;  
+	private int dia;
 	
 	
+Tiempo(){
+	this.hora=07;
+	this.dias = new String[7];
+	this.dias [0] = "Lunes";
+	this.dias [1] = "Martes";
+	this.dias [2] = "Miercoles";
+	this.dias [3] = "Jueves";
+	this.dias [4] = "Viernes";
+	this.dias [5] = "Sabado";
+	this.dias [6] = "Domingo";
+	this.dia = 0;
+}
+
+public void consumirTiempo (int tiemp){
+	this.hora = hora + tiemp;
+	if (this.hora >= 21){
+		//policia.dormir;
+	}
+	else if (this.hora >= 24){
+		this.hora = this.hora - 24;
+		this.aumentoDia();
+	}
+}
+public void aumentoDia(){
+	this.dia = this.dia +1;
+}
+
+public boolean terminoTiempo(){
+	if (this.dia == 6 && hora >=17){
+		return true;
+	}
+		else return false;
+}
+
+
+public String getDia(){
+	return this.dias[this.dia];
+}
+
+public int getHora(){
+	return this.hora;
+}
 
 }
